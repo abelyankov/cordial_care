be58546a031b39eaf2aca9cc46769d7df4fcac65
Arthur <arthurbelyankove@gmail.com>
2019-08-07 01:34:16 +0600

    routes edited

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
 
   devise_for :members, skip: [:sessions, :registrations]
   devise_scope :member do
-    get 'erp/login', to: 'devise/sessions#new', as: :new_member_session
-    post 'erp/login', to: 'devise/sessions#create', as: :member_session
-    delete 'erp/logout', to: 'devise/sessions#destroy', as: :destroy_member_session
-    get 'erp/members/edit' => 'devise/registrations#edit', as: :edit_member_registration
-    put 'erp/members' => 'devise/registrations#update', as: :member_registration
+    get 'login', to: 'devise/sessions#new', as: :new_member_session
+    post 'login', to: 'devise/sessions#create', as: :member_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :destroy_member_session
+    get 'members/edit' => 'devise/registrations#edit', as: :edit_member_registration
+    put 'members' => 'devise/registrations#update', as: :member_registration
   end
 
-  scope module: 'erp', path: 'erp' do
+  scope module: 'erp' do
     root 'sales#index'
     get '/sales/type' => 'sales#select_type'
     get '/sales/type/:sale_type_id/new' => 'sales#new', as: :sales_type_new
